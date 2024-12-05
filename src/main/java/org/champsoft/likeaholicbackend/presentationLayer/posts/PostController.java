4f9adcae74197ed35bbf4014ef0c10b5d8a5b9b1
AmirGHa69 <sraldon24@outlook.com>
2024-12-04 22:39:48 -0500

    v13

--- src/main/java/org/champsoft/likeaholicbackend/presentationLayer/posts/PostController.java
+++ src/main/java/org/champsoft/likeaholicbackend/presentationLayer/posts/PostController.java
@@ -2,7 +2,10 @@ package org.champsoft.likeaholicbackend.presentationLayer.posts;
 
 import org.champsoft.likeaholicbackend.businessLogicLayer.PostService;
 import org.champsoft.likeaholicbackend.dataAccessLayer.Post;
+import org.champsoft.likeaholicbackend.dataAccessLayer.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,18 +15,36 @@ import java.util.List;
 public class PostController {
     @Autowired
     private PostService postService;
+    @Autowired
+    PostRepository postRepository;
 
     @PostMapping
     public PostResponseModel createPost(@RequestBody PostRequestModel postRequestModel) {
         return postService.createPost(postRequestModel);
     }
 
-    @PutMapping("/{id}")
-    public Post updatePost(@PathVariable String postId, @RequestBody Post post) {
-        return postService.updatePost(postId, post);
+    @PutMapping("/{postId}")
+    public ResponseEntity<?> updatePost(@PathVariable String postId, @RequestBody PostRequestModel postRequest) {
+        Post optionalPost = postRepository.findByPostId(postId);
+        if (optionalPost != null) {
+
+            // Authorization check
+            if (!optionalPost.getUser().getUserId().equals(postRequest.getUserId())) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You are not authorized to update this post.");
+            }
+
+            // Update post fields
+            optionalPost.setContent(postRequest.getContent());
+            optionalPost.setImageUrl(postRequest.getImageUrl());
+            postRepository.save(optionalPost);
+
+            return ResponseEntity.ok("Post updated successfully");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Post not found");
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/{postId}")
     public String deletePost(@PathVariable String postId) {
         System.out.println("postId in controller: " + postId);
         return postService.deletePost(postId);
